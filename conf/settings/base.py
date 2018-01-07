e4ea91025af96b45a6eb7da5e663fec71f73f417
Jason Mize <jasonemize@gmail.com>
2018-01-06 22:41:57 -0500

    fix to secret key

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -16,6 +16,7 @@ MEDIA_URL = '/media/'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = []
 
