e192d5b0dbe566ab69bc8b1884295e9dac1e95d8
Jason Mize <jasonemize@gmail.com>
2018-01-07 22:08:22 -0500

    fixed migrations

--- comic/migrations/0010_auto_20170107_1559.py
+++ comic/migrations/0010_auto_20170107_1559.py
@@ -9,12 +9,20 @@ class Migration(migrations.Migration):
     dependencies = [
         ('comic', '0009_auto_20170107_1529'),
     ]
-
     operations = [
+        migrations.CreateModel(
+            name='Issue',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('title', models.CharField(null=True, blank=True, max_length=200)),
+                ('image', models.ImageField(null=True, blank=True, upload_to='issues')),
+                ('issue_number', models.IntegerField(blank=True, null=True)),
+            ],
+        ),
         migrations.AlterField(
             model_name='comic',
             name='issue',
-            field=models.IntegerField(null=True, blank=True),
+            field=models.ForeignKey(null=True, to='comic.Issue', blank=True),
         ),
         migrations.AlterField(
             model_name='comic',
