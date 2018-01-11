1aea1ca28981dc2f8ae73746a769939a4c31a7b4
Jason Mize <jasonemize@gmail.com>
2018-01-10 19:56:37 -0500

    fix

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -20,10 +20,6 @@ ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
-# INSTALLED_APPS += [
-    # 'storages'
-# ]
-
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
