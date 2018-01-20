260e97b78464639a23058e411c75ce90ac04c16b
Jason Mize <jasonemize@gmail.com>
2018-01-20 15:35:40 -0500

    prod fix

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -23,6 +23,7 @@ DEBUG = False
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
@@ -38,4 +39,8 @@ MEDIA_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'conf.settings.storage_backends.MediaStorage'
+
+
+
+
 
