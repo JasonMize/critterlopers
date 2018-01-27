b2085a08c8d5256d37bef85ad9e98b38eae14979
Jason Mize <jasonemize@gmail.com>
2018-01-27 14:27:10 -0500

    updates

--- conf/urls.py
+++ conf/urls.py
@@ -9,17 +9,10 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('rest_framework.urls')),
+    # url(r'^api/', include('rest_framework.urls')),
     url(r'^api/', include('api.urls')),
     url(r'^comic/', include('comic.urls')),
     url(r'^', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns += [
-#         url(r'^__debug__/', include(debug_toolbar.urls)),
-#     ]
 
