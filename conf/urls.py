0ad317365237baf1e97c3473d2246c238df5faea
Jason Mize <jasonemize@gmail.com>
2018-02-19 18:24:30 -0500

    test url

--- conf/urls.py
+++ conf/urls.py
@@ -11,9 +11,9 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('api.urls')),
-    url(r'^comic/', include('comic.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+    url(r'^comic/', include('comic.urls')),
+    url(r'^api/', include('api.urls')),
     # TODO make last page update automatically
     url(r'^$', RedirectView.as_view(url='/comic/38', permanent=False)),
     url(r'^', include('core.urls')),
