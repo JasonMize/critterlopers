84dd782554358a6b8c6fbb6da55dc2b458ad08f8
Jason Mize <jasonemize@gmail.com>
2018-03-23 11:59:43 -0400

    added components, routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/App.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Comic from './components/Comic';
+import About from './components/About';
+import Archive from './components/Archive';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path="/comic" component={Comic} />
+      <Route path="/about" component={About} />
+      <Route path="/archive" component={Archive} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
+
